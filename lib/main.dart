73a0ddec037f483edb3a4e50e0392841b79d1610
Shubham <shubhamkumarwinner@gmail.com>
2021-05-24 23:29:06 +0530

    Simple reactive state management using obx

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_x/home.dart';
-import 'package:get_x/next_screen.dart';
-import 'package:get_x/unknown_route.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  var count = 0.obs;
+  void increment() {
+    count++;
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Go to Home',
-      initialRoute: '/',
-      defaultTransition: Transition.zoom,
-      getPages: [
-        GetPage(name: '/', page: () => MyApp()),
-        GetPage(name: '/home', page: () => Home()),
-        // GetPage(
-        //     name: '/nextScreen',
-        //     page: () => NextScreen(),
-        //     transition: Transition.leftToRight),
-        GetPage(
-            name: '/nextScreen/:someValue',
-            page: () => NextScreen(),
-            transition: Transition.leftToRight),
-      ],
-      unknownRoute: GetPage(name: '/notfound', page: () => UnknownRoute()),
+      title: 'State Management',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Go to Home'),
+          title: Text('State Management'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
+            children: [
+              /**
+               * Using Obx for state management
+               */
+              Obx(() => Text(
+                    'Count Value is $count',
+                    style: TextStyle(fontSize: 25),
+                  )),
+              SizedBox(height: 10),
               TextButton(
-                onPressed: () {
-                  Get.toNamed('/home');
-
-                  //if name is not found unknown route is opened
-                  // Get.toNamed('/x');
-
-                  // Get.offNamed('/home');
-                  // Get.offAllNamed('/home');
-                  // Get.toNamed(
-                  //     '/home?channel=Ripples Code&content=Flutter GetX');
-                },
-                child: Text('Go to Home'),
+                onPressed: increment,
+                child: Text('Increment'),
                 style: TextButton.styleFrom(
                   primary: Colors.white,
                   backgroundColor: Colors.blue,
