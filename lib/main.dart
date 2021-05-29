6caa0e2f874990c3a096c48830e9d5f9c2258349
Shubham <shubhamkumarwinner@gmail.com>
2021-05-29 23:10:39 +0530

    GetView in GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,47 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
+import 'package:get_x/count_controller.dart';
 
-Future<void> main() async {
-  // Initialize storage driver
-  await GetStorage.init();
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  var emailEditingController = TextEditingController();
-  var storage = GetStorage();
+//GetView
+// =======
+// If we have single controller as a dependency then we can use GetView
+//instead of StatelessWidget and avoid writing Get.find
+
+//GetWidget
+// ========
+//It is similar to GetView with one difference it gives the same instance of
+//Get.find everytime. It becomes very useful when used in combination with
+//Get.create
+
+class MyApp extends GetView<CountController> {
   @override
   Widget build(BuildContext context) {
+    Get.put(CountController());
+    // Get.create(() => CountController());
     return GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'GetStorage & Email Validation',
+        title: 'GetView',
         theme: ThemeData(
           primarySwatch: Colors.lightBlue,
         ),
         home: Scaffold(
           appBar: AppBar(
-            title: Text('GetStorage & Email Validation'),
+            title: Text('GetView'),
           ),
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: emailEditingController,
-                  ),
-                ),
+                Obx(() => Text(
+                      'The value is ${controller.count}',
+                      style: TextStyle(fontSize: 25),
+                    )),
                 SizedBox(
                   height: 8,
                 ),
@@ -42,36 +49,15 @@ class MyApp extends StatelessWidget {
                   padding: EdgeInsets.all(16.0),
                   child: TextButton(
                     onPressed: () {
-                      if (GetUtils.isEmail(emailEditingController.text)) {
-                        storage.write('email', emailEditingController.text);
-                      } else {
-                        Get.snackbar(
-                            'Incorrect Email', 'Provide Email in valid format',
-                            colorText: Colors.white,
-                            backgroundColor: Colors.red,
-                            snackPosition: SnackPosition.BOTTOM);
-                      }
+                      print(controller.hashCode);
+                      controller.increment();
                     },
-                    child: Text('Write'),
+                    child: Text('Increment'),
                     style: TextButton.styleFrom(
                       primary: Colors.white,
                       backgroundColor: Colors.blue,
                       padding: EdgeInsets.symmetric(horizontal: 20),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                TextButton(
-                  onPressed: () {
-                    print('The Email is ${storage.read('email')}');
-                  },
-                  child: Text('Read'),
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Colors.blue,
-                    padding: EdgeInsets.symmetric(horizontal: 20),
                   ),
                 ),
               ],
@@ -80,22 +66,3 @@ class MyApp extends StatelessWidget {
         ));
   }
 }
-
-// To listen for changes
-//var listen = storage.listen(()=> print('Email Changed'););
-
-// when subscribed to listen event it should be disposed by using
-// storage.removeListen(listen);
-
-// To listen for changes in key
-// storage.listenKey('email', (value){ print('new key is $value');});
-
-//Remove a key
-// storage.remove('email');
-
-//Erase the container
-//storage.erase();
-
-// create container with a name
-//GetStorage g = GetStorage('MyStorage');
-// await GetStorage.init('MyStorage');
