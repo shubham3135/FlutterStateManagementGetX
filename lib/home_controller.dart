5c10e1d9dfe0d8a47e7d958a53604fbadb70b48a
Shubham <shubhamkumarwinner@gmail.com>
2021-06-02 01:21:09 +0530

    Draggable Scrollable sheet

--- lib/home_controller.dart
+++ lib/home_controller.dart
@@ -1,41 +1,5 @@
-import 'package:chewie/chewie.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:video_player/video_player.dart';
 
 class HomeController extends GetxController {
-  late VideoPlayerController videoPlayerController;
-  ChewieController? chewieController;
-
-  @override
-  void onInit() {
-    super.onInit();
-    initializePlayer();
-  }
-
-  @override
-  void onClose() {
-    videoPlayerController.dispose();
-    chewieController!.dispose();
-  }
-
-  Future<void> initializePlayer() async {
-    videoPlayerController = VideoPlayerController.network(
-        'https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4');
-    await Future.wait([videoPlayerController.initialize()]);
-    chewieController = ChewieController(
-        videoPlayerController: videoPlayerController,
-        autoPlay: true,
-        looping: true,
-        materialProgressColors: ChewieProgressColors(
-            playedColor: Colors.red,
-            handleColor: Colors.cyanAccent,
-            backgroundColor: Colors.yellow,
-            bufferedColor: Colors.lightGreen),
-        placeholder: Container(
-          color: Colors.greenAccent,
-        ),
-        autoInitialize: true);
-    update();
-  }
+  List<bool> selected = List.generate(40, (index) => false).obs;
 }
--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,39 +1,88 @@
-import 'package:chewie/chewie.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_x/home_controller.dart';
 
 class HomeView extends StatelessWidget {
+  HomeController controller = Get.put<HomeController>(HomeController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Video Player'),
+        title: Text('Draggable Scrollable Sheet'),
         centerTitle: true,
       ),
-      body: Column(
+      body: Stack(
         children: [
-          GetBuilder<HomeController>(
-              init: HomeController(),
-              builder: (controller) => Expanded(
-                    child: Center(
-                      child: (controller.chewieController != null &&
-                              controller.chewieController!.videoPlayerController
-                                  .value.isInitialized)
-                          ? Chewie(controller: controller.chewieController!)
-                          : Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                CircularProgressIndicator(),
-                                SizedBox(
-                                  height: 10,
+          Container(
+            width: context.width,
+            height: context.height,
+            color: Colors.purple,
+            child: Padding(
+              padding: EdgeInsets.only(left: 16, right: 16, top: 20),
+              child: Text(
+                'Draggable Scrollable Sheet',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          DraggableScrollableSheet(
+            builder:
+                (BuildContext buildContext, ScrollController scrollController) {
+              return Padding(
+                padding: EdgeInsets.all(16),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white),
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext buildContext, int index) {
+                      return Obx(() => Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: controller.selected[index]
+                                    ? Colors.blue
+                                    : null),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.lightBlue,
+                                child: Text(
+                                  index.toString(),
+                                  style: TextStyle(color: Colors.white),
                                 ),
-                                Text('Loading...'),
-                              ],
+                              ),
+                              title: Text(
+                                'Index position is $index',
+                                style: TextStyle(
+                                    color: controller.selected[index]
+                                        ? Colors.white
+                                        : Colors.black),
+                              ),
+                              tileColor: Colors.green,
+                              // selectedTileColor: controller.selected[index]
+                              //     ? Colors.green
+                              //     : Colors.transparent,
+                              selected: controller.selected[index],
+                              onTap: () => controller.selected[index] =
+                                  !controller.selected[index],
                             ),
-                    ),
-                  ))
+                          ));
+                    },
+                    itemCount: 40,
+                    controller: scrollController,
+                  ),
+                ),
+              );
+            },
+            initialChildSize: .3,
+            minChildSize: .15,
+            maxChildSize: 0.8,
+          )
         ],
       ),
     );
